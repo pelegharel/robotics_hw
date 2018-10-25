4316851f79dc42949de1775ea31ba8a5b92d40dc
peleg <a@a.com>
2018-10-25 14:54:07 +0300

    create a service

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,8 +7,4 @@
 # new environment variables
 
 # modified environment variables
-export CMAKE_PREFIX_PATH="/home/peleg/catkin_ws/devel:/opt/ros/kinetic"
-export LD_LIBRARY_PATH="/home/peleg/catkin_ws/devel/lib:/opt/ros/kinetic/lib:/opt/ros/kinetic/lib/x86_64-linux-gnu"
-export PKG_CONFIG_PATH="/home/peleg/catkin_ws/devel/lib/pkgconfig:/opt/ros/kinetic/lib/pkgconfig:/opt/ros/kinetic/lib/x86_64-linux-gnu/pkgconfig"
 export PWD="/home/peleg/catkin_ws/build"
-export ROS_PACKAGE_PATH="/home/peleg/catkin_ws/src:/opt/ros/kinetic/share"
