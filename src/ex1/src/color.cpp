d2a7c34ddd95a48dbc37c9d4214ad9d55dc70a4b
peleg <a@a.com>
2018-11-02 16:32:56 +0200

    keyboard output

--- src/ex1/src/color.cpp
+++ src/ex1/src/color.cpp
@@ -2,6 +2,7 @@
 #include "std_msgs/ColorRGBA.h"
 #include <iostream>
 #include <tuple>
+#include <utility>
 
 std::istream &operator>>(std::istream &is, std_msgs::ColorRGBA &c) {
   is >> c.r >> c.g >> c.b >> c.a;
@@ -19,20 +20,27 @@ int main(int argc, char **argv) {
     std::tie(m.r, m.g, m.b, m.a) = std::make_tuple(1, 2, 3, 4);
     return m;
   }();
-  ros::Rate loop_rate(10);
 
   while (ros::ok()) {
-    const auto msg = []{
+    const auto msg_opt = [] {
       std_msgs::ColorRGBA m;
       std::cout << "Insert a color 'r g b a':" << '\n';
-      std::cin >> m;
-      return m;
+      if (std::cin >> m) {
+        return std::make_pair(true, m);
+      } else {
+        return std::make_pair(false, m);
+      }
     }();
-   
+
+    if (!msg_opt.first) {
+      return 0;
+    }
+
+    const auto msg = msg_opt.second;
+
     ROS_INFO("publish: (%f %f %f %f)", msg.r, msg.g, msg.b, msg.a);
     color_publisher.publish(msg);
     ros::spinOnce();
-    loop_rate.sleep();
   }
   return 0;
 }
