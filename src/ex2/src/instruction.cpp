b31538df9a561aa14d6b098818927c774635d913
peleg <a@a.com>
2018-11-24 20:47:07 +0200

    image and info transport

--- src/ex2/src/instruction.cpp
+++ src/ex2/src/instruction.cpp
@@ -1,10 +1,16 @@
 #include "boost/optional.hpp"
+#include "cv_bridge/cv_bridge.h"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
 #include "ros/ros.h"
 #include <boost/math/constants/constants.hpp>
 #include <functional>
 #include <geometry_msgs/Twist.h>
 #include <geometry_msgs/Vector3.h>
+#include <image_geometry/pinhole_camera_model.h>
+#include <image_transport/image_transport.h>`
 #include <iostream>
+#include <sensor_msgs/Image.h>
 #include <sensor_msgs/LaserScan.h>
 #include <stdexcept>
 #include <string>
@@ -12,6 +18,7 @@
 #include <utility>
 
 using namespace std;
+using namespace cv;
 
 struct Publishers {
   ros::Publisher velocity;
@@ -20,17 +27,39 @@ struct Publishers {
 class TurtleBotSensors {
 private:
   ros::Subscriber laser_sub;
+  void recieved_scan(const sensor_msgs::LaserScan::ConstPtr &msg) {
+    last_scan.emplace(*msg);
+  }
+
 public:
   TurtleBotSensors(ros::NodeHandle &n)
       : laser_sub(n.subscribe("scan", 1000, &TurtleBotSensors::recieved_scan,
                               this)) {}
 
   boost::optional<sensor_msgs::LaserScan> last_scan;
+};
 
-  void recieved_scan(const sensor_msgs::LaserScan::ConstPtr &msg) {
-    last_scan.emplace(*msg);
+class Camera {
+private:
+  image_transport::CameraSubscriber camera_sub;
+  void recieved_image(const sensor_msgs::ImageConstPtr &image_msg,
+                      const sensor_msgs::CameraInfoConstPtr &info_msg) {
+    auto cv_ptr =
+        cv_bridge::toCvCopy(image_msg, sensor_msgs::image_encodings::RGB8);
+    last_image.emplace(cv_ptr->image);
+    last_model = image_geometry::PinholeCameraModel();
+    last_model->fromCameraInfo(info_msg);
+
+    ROS_INFO("CAMERA update");
   }
 
+public:
+  boost::optional<Mat> last_image;
+  boost::optional<image_geometry::PinholeCameraModel> last_model;
+
+  Camera(image_transport::ImageTransport &it)
+      : camera_sub(it.subscribeCamera("camera/image_raw", 1,
+                                      &Camera::recieved_image, this)) {}
 };
 
 auto fwd_speed_msg(double m_p_sec) {
@@ -70,6 +99,8 @@ void rotate_clockwise(ros::Publisher &velocity, double alpha) {
   speed_for_time(velocity, clk_speed_msg(alpha / 4), 4);
 }
 
+void dist_color() {}
+
 string input_text() {
   return R"(
 Plesae enter number of one of the following commands:
@@ -84,7 +115,10 @@ Plesae enter number of one of the following commands:
 int main(int argc, char **argv) {
   ros::init(argc, argv, "instruction");
   ros::NodeHandle n;
+  image_transport::ImageTransport it(n);
+
   TurtleBotSensors sensors(n);
+  Camera camera(it);
 
   Publishers publishers{n.advertise<geometry_msgs::Twist>("cmd_vel", 1000)};
 
@@ -107,10 +141,11 @@ int main(int argc, char **argv) {
           return;
         }
         switch (command) {
-        case 1:
+        case 1: {
           ros::spinOnce();
           return move_forward(*sensors.last_scan, publishers.velocity, 0.05);
-        case 2:
+        }
+        case 2: {
           cout << "Enter alpha:\n";
           const double alpha = [] {
             string s;
@@ -120,6 +155,10 @@ int main(int argc, char **argv) {
           return rotate_clockwise(
               publishers.velocity,
               alpha * (boost::math::constants::pi<double>() / 180.0));
+        }
+        case 3: {
+          return dist_color();
+        }
       }
       }();
 
